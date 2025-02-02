1c8628fd2a4a709ea1343b83eff9510ef5811006
Soucius <soucius.ggwp@gmail.com>
2025-02-03 00:58:30 +0300

    first commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return (
+    <div className="card">
+      <h2>{ title }</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "🧡" : "🤍"}
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  );
+};
+
+export default App;
